05fa7cef5432e81722e36533092fd60534c86eb4
Enock Koech <ekipkorir2015@gmail.com>
2022-12-08 22:51:05 +0300

    modified and added the necessary files

---
+++ src/components/pages/Home.js
@@ -0,0 +1,16 @@
+import "../../App.css";
+import Cards from "../Cards";
+import HeroSection from "../HeroSection";
+import Footer from "./Footer";
+
+const Home = () => {
+  return (
+    <>
+      <HeroSection />
+      <Cards />
+      <Footer />
+    </>
+  );
+};
+
+export default Home;
